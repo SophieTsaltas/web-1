34b7534b2b399b498c30953e01bd98ba0e5f4101
SophieTsaltas <sophietsaltas@gmail.com>
2024-10-28 20:41:05 -0300

    Add files via upload

---
+++ type-workshop/js/type.js
@@ -0,0 +1,95 @@
+function myFunction() {
+    var moreText = document.getElementById("more1");
+
+    if (moreText.style.display === "inline") {
+        moreText.style.display = "none";
+    } else {
+        moreText.style.display = "inline";
+        window.location.href = "#one";
+    }
+}
+function myFunction2() {
+    var moreText2 = document.getElementById("more2");
+
+    if (moreText2.style.display === "none") {
+        moreText2.style.display = "inline";
+    } else {
+        moreText2.style.display = "none";
+        window.location.href = "#two";
+    }
+}
+
+function myFunction3() {
+    var moreText3 = document.getElementById("more3");
+
+    if (moreText3.style.display === "none") {
+        moreText3.style.display = "inline";
+    } else {
+        moreText3.style.display = "none";
+        window.location.href = "#three";
+    }
+}
+
+function myFunction4() {
+    var moreText4 = document.getElementById("more4");
+
+    if (moreText4.style.display === "none") {
+        moreText4.style.display = "inline";
+    } else {
+        moreText4.style.display = "none";
+        window.location.href = "#four";
+    }
+}
+
+function myFunction5() {
+    var moreText5 = document.getElementById("more5");
+
+    if (moreText5.style.display === "none") {
+        moreText5.style.display = "inline";
+    } else {
+        moreText5.style.display = "none";
+        window.location.href = "#five";
+    }
+}
+
+function myFunction6() {
+    var moreText6 = document.getElementById("more6");
+
+    if (moreText6.style.display === "none") {
+        moreText6.style.display = "inline";
+    } else {
+        moreText6.style.display = "none";
+        window.location.href = "#six";
+    }
+}
+function myFunction7() {
+    var moreText7 = document.getElementById("more7");
+
+    if (moreText7.style.display === "none") {
+        moreText7.style.display = "inline";
+    } else {
+        moreText7.style.display = "none";
+        window.location.href = "#seven";
+    }
+}
+function myFunction8() {
+    var moreText8 = document.getElementById("more8");
+
+    if (moreText8.style.display === "none") {
+        moreText8.style.display = "inline";
+    } else {
+        moreText8.style.display = "none";
+        window.location.href = "#eight";
+    }
+}
+
+function myFunction9() {
+    var moreText9 = document.getElementById("more9");
+
+    if (moreText9.style.display === "none") {
+        moreText9.style.display = "inline";
+    } else {
+        moreText9.style.display = "none";
+        window.location.href = "#nine";
+    }
+}
